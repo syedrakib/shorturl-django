acbaa1549813c4c7741e6136b2827d700509afc6
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-03-31 02:13:30 +0600

    Implement helpers.is_valid_URL()

--- url_service/helpers.py
+++ url_service/helpers.py
@@ -1,3 +1,5 @@
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 
 def HTML_http_response(status_code, HTML_body):
@@ -6,3 +8,12 @@ def HTML_http_response(status_code, HTML_body):
 		content_type = "text/html",
 		status = status_code
 	)
+
+def is_valid_URL(a_url):
+	validate = URLValidator()
+	try:
+		validate(a_url)
+	except ValidationError:
+		return False
+	else:
+		return True
