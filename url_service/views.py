28c0fe6ecb2597a9482d76a98f88c44abea03c67
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-03-31 02:47:35 +0600

    Implement URL shortening processing

--- url_service/views.py
+++ url_service/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .helpers import HTML_http_response
+from .helpers import HTML_http_response, generate_random_string
+from .models import URLMapper
 
 # Create your views here.
 def index(request):
@@ -10,7 +12,25 @@ def index(request):
 			template_name='shortener_form.html'
 		)
 	elif request.method == "POST":
-		return HTML_http_response(200, "<b>process form</b>")
+		long_url = request.POST['long_url'].strip()
+		try:
+			# if this long_url is alrady present in our DB, we won't create another short URL for it
+			urlmapper_obj = URLMapper.objects.get(long_url=long_url)
+		except URLMapper.DoesNotExist:
+			created = False
+			while not created:
+				# In highly improbable / unlikely cases,
+				# if our generated random string collides with an already existing short_url in DB,
+				# we will keep retrying until get_or_create() returns created=True
+				urlmapper_obj, created = URLMapper.objects.get_or_create(
+					short_url=generate_random_string(7),
+					defaults={
+						'long_url': long_url
+					}
+				)
+		return HttpResponseRedirect(
+			reverse('url_service:inflate', args=(urlmapper_obj.short_url,))
+		)
 	else:
 		return HTML_http_response(200, "<b>restricted attempt</b>")
 
