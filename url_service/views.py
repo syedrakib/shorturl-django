b661360f8efb2b5c686dc95edc09e29e155f05f8
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-03-31 02:51:54 +0600

    Check is_valid_URL before proceeding to shorten a long URL

--- url_service/views.py
+++ url_service/views.py
@@ -1,7 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from .helpers import HTML_http_response, generate_random_string
+from .helpers import HTML_http_response, generate_random_string, is_valid_URL
 from .models import URLMapper
 
 # Create your views here.
@@ -13,24 +13,31 @@ def index(request):
 		)
 	elif request.method == "POST":
 		long_url = request.POST['long_url'].strip()
-		try:
-			# if this long_url is alrady present in our DB, we won't create another short URL for it
-			urlmapper_obj = URLMapper.objects.get(long_url=long_url)
-		except URLMapper.DoesNotExist:
-			created = False
-			while not created:
-				# In highly improbable / unlikely cases,
-				# if our generated random string collides with an already existing short_url in DB,
-				# we will keep retrying until get_or_create() returns created=True
-				urlmapper_obj, created = URLMapper.objects.get_or_create(
-					short_url=generate_random_string(7),
-					defaults={
-						'long_url': long_url
-					}
-				)
-		return HttpResponseRedirect(
-			reverse('url_service:inflate', args=(urlmapper_obj.short_url,))
-		)
+		if not is_valid_URL(long_url):
+			return render(
+				request=request, 
+				template_name='shortener_form.html', 
+				status=400,
+			)
+		else:
+			try:
+				# if this long_url is alrady present in our DB, we won't create another short URL for it
+				urlmapper_obj = URLMapper.objects.get(long_url=long_url)
+			except URLMapper.DoesNotExist:
+				created = False
+				while not created:
+					# In highly improbable / unlikely cases,
+					# if our generated random string collides with an already existing short_url in DB,
+					# we will keep retrying until get_or_create() returns created=True
+					urlmapper_obj, created = URLMapper.objects.get_or_create(
+						short_url=generate_random_string(7),
+						defaults={
+							'long_url': long_url
+						}
+					)
+			return HttpResponseRedirect(
+				reverse('url_service:inflate', args=(urlmapper_obj.short_url,))
+			)
 	else:
 		return HTML_http_response(200, "<b>restricted attempt</b>")
 
