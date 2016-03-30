6e51fd610cf33a4163599ce08657cd4698ff826c
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-03-31 00:19:35 +0600

    Create URLMapper model
    This model will contain the real mappings of short_url to long_url

--- url_service/models.py
+++ url_service/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class URLMapper(models.Model):
+	short_url = models.CharField(max_length=16, unique=True)
+	long_url = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		return "%d-%s-%s" % (self.pk, self.short_url, self.long_url)
