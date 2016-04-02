229135e40e1c5ecfc04503af034b5d81c08a51ed
Syed Rakib Al Hasan <syed.rakib.al.hasan@gmail.com>
2016-04-03 01:06:34 +0600

    Write test case for generate_random_string() helper

--- url_service/tests.py
+++ url_service/tests.py
@@ -1,9 +1,13 @@
 from nose.tools import eq_
 from mock import patch
-from .helpers import is_valid_URL
+from .helpers import is_valid_URL, generate_random_string
+import re
 
 def test_is_valid_URL():
 	eq_(is_valid_URL("facebook"), False)
 	eq_(is_valid_URL("facebook.com"), False)
 	eq_(is_valid_URL("www.facebook.com"), False)
 	eq_(is_valid_URL("http://www.facebook.com"), True)
+
+def test_generate_random_string():
+	eq_(len(re.findall('[^a-zA-Z0-9]', generate_random_string())) == 0, True)
